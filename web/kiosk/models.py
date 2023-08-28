cb01802e97f1674f03ddce7544479a0697b32aef
realisshoon <hsgn21@naver.com>
2023-08-28 16:47:39 +0900

    kiosk model 생성, view.py 수정 필요

--- web/kiosk/models.py
+++ web/kiosk/models.py
@@ -1,17 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Kiosk(models.Model):
     id=models.AutoField(primary_key=True, unique=True)
+    store_id=models.IntegerField(models.ForeignKey("app.Model", on_delete=models.CASCADE))
+    name = models.CharField(max_length = 32, verbose_name="상품명")
+    price = models.IntegerField(verbose_name = "상품가격")
+    stock = models.IntegerField(verbose_name="수량")
+    registered_date = models.DateTimeField(verbose_name="등록시간", auto_now_add=timezone.now)
+
+    # def get_name(self):
+    #     return {'person_id': self.name,'price':self.price,'stock':self.stock}
+
+    # def get_date(self):
+    #     return self.registered_date
+
+    # def __str__(self):
+    #     return self.name
+    
+
+    class Meta:
+        db_table = "kiosk"
+        verbose_name = "상품"
+        verbose_name_plural = "상품"
 
 
-class TodayTraffic(models.Model):
-    person_id = models.IntegerField(default=-1, primary_key=True)
-    date = models.DateField()
-    time = models.TimeField()
 
-    def get_person_id(self):
-        return {'person_id': self.person_id}
-
-    def get_month(self):
-        return self.date.strftime("%m")
--- web/kiosk/views.py
+++ web/kiosk/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Kiosk
+from django.utils import timezone
 # Create your views here.
+
+def index(request):
+    q = Kiosk(id,store_id=1,name="ramen",price=1000,stock=1, registered_date=timezone.now())
+    q.save()
+    return HttpResponse(f"{Kiosk.id}{q}{Kiosk.registered_date}")
