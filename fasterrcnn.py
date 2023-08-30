1f95c0bcec6be49050e8bd240c823da4f4969de8
chanhaebang <bh2593@gmail.com>
2023-08-30 16:37:59 +0900

    'fasterrcnn_수정'

--- fasterrcnn.py
+++ fasterrcnn.py
@@ -1,5 +1,4 @@
 import os
-import torch
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import torchvision
@@ -11,6 +10,8 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
 import numpy as np
+from sklearn.metrics import average_precision_score
+import torch
 
 
 class YOLOLabelTransform:
@@ -70,11 +71,11 @@ class CustomDataset(Dataset):
 
         transform = A.Compose([
             A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-            A.GaussianBlur(blur_limit=3),
-            A.MotionBlur(blur_limit=5),
+            A.GaussianBlur(blur_limit=(3, 7)),
+            A.MotionBlur(blur_limit=(3, 7)),
             A.HorizontalFlip(),
-            ToTensorV2(),
-            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ToTensorV2()
         ])
 
         augmented = transform(image=np.array(image))
@@ -88,15 +89,37 @@ class CustomDataset(Dataset):
 
         return image, transformed_targets
 
+def calculate_mAP(y_true, y_pred, num_classes):
+    """
+    Calculate mAP for multiple classes
+    """
+    APs = []
+
+    for class_idx in range(num_classes):
+        preds = y_pred[y_true[:, -1] == class_idx]
+        labels = y_true[y_true[:, -1] == class_idx][:, :-1]
+
+        if len(labels) == 0:
+            continue
+
+        AP = average_precision_score(labels, preds)
+        APs.append(AP)
+
+    mAP = np.mean(APs)
+    return mAP
+
 
 
 # 사용 예시
 image_dir = 'ultralytics/cfg/cctv2nd_dataset/train/images'
 label_dir = 'ultralytics/cfg/cctv2nd_dataset/train/labels'
+val_image_dir = 'ultralytics/cfg/cctv2nd_dataset/val/images'
+val_label_dir = 'ultralytics/cfg/cctv2nd_dataset/val/labels'
 num_classes = 7  # 클래스 개수에 맞게 수정
 img_size = (640, 640)  # 이미지 크기에 맞게 수정
 
 dataset = CustomDataset(image_dir, label_dir, num_classes, img_size)
+val_dataset = CustomDataset(val_image_dir, val_label_dir, num_classes, img_size)
 def collate_fn(batch):
     images, targets_list = zip(*batch)
     transformed_targets_list = []
@@ -121,14 +144,15 @@ def collate_fn(batch):
 
 
 
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True, collate_fn=collate_fn)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
 
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load a pre-trained Faster R-CNN model
 def get_model(num_classes):
     # Load a pre-trained ResNet-50 model
-    backbone = torchvision.models.detection.backbone_utils.resnet_fpn_backbone('resnet50', pretrained=True)
+    backbone = torchvision.models.detection.backbone_utils.resnet_fpn_backbone(backbone_name='resnet50', weights=None)
 
     # Define the number of anchor boxes and their sizes
     rpn_anchor_generator = AnchorGenerator(
@@ -148,6 +172,7 @@ def get_model(num_classes):
 
 # Initialize the model
 model = get_model(num_classes)
+model.to(device)
 
 # Define your optimizer and learning rate scheduler
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.0005)
@@ -163,11 +188,11 @@ for epoch in range(num_epochs):
     epoch_losses = []
 
     for images, targets_list in tqdm(dataloader, desc=f"Epoch [{epoch + 1}/{num_epochs}]"):
-        images = [F.to_tensor(img) for img in images]
+        # 이미지와 타겟을 디바이스로 이동
+        images = [img.to(device) for img in images]
+        targets_list = [{k: v.to(device) for k, v in t.items()} for t in targets_list]
 
-        targets = targets_list
-
-        loss_dict = model(images, targets)
+        loss_dict = model(images, targets_list)
         losses = sum(loss for loss in loss_dict.values())
 
         optimizer.zero_grad()
@@ -182,12 +207,47 @@ for epoch in range(num_epochs):
     # 검증 세트에서 모델 평가
     model.eval()
     with torch.no_grad():
-        # ... 검증 세트에서 추론을 수행하고 mAP를 계산합니다.
-        # 실제 mAP 계산 코드로 아래 줄을 대체하세요.
-        val_mAP = 0.5
 
-    if val_mAP > best_mAP:
-        best_mAP = val_mAP
-        # 최적 모델의 파라미터 저장
-        torch.save(model.state_dict(), best_model_path)
-        print(f"Best model saved with mAP: {best_mAP}")
+        # Initialize variables to store true and predicted labels
+        all_y_true = []
+        all_y_pred = []
+
+        # Initialize variable to store mAP
+        val_mAP = 0.0
+
+        # Count of batches
+        count = 0
+
+        with torch.no_grad():
+            for batch in val_dataloader:  # Assume val_dataloader is your validation data loader
+                images, targets = batch  # Assume each batch returns images and targets
+
+                # Forward pass through the model
+                outputs = model(images)
+
+                # Here you'll need to extract the predicted and true labels from outputs and targets
+                # This highly depends on how your data and model are structured
+                y_pred = outputs['labels'].cpu().numpy()  # Dummy code, replace with your actual code
+                y_true = targets['labels'].cpu().numpy()  # Dummy code, replace with your actual code
+
+                # Append to the list
+                all_y_pred.extend(y_pred)
+                all_y_true.extend(y_true)
+
+                # Compute mAP for this batch and accumulate
+                batch_mAP = calculate_mAP(y_true, y_pred)
+                val_mAP += batch_mAP
+
+                count += 1
+
+            # Average over all batches to get the final mAP
+            val_mAP /= count
+
+        print(f"Validation mAP: {val_mAP}")
+
+
+        if val_mAP > best_mAP:
+            best_mAP = val_mAP
+            # 최적 모델의 파라미터 저장
+            torch.save(model.state_dict(), best_model_path)
+            print(f"Best model saved with mAP: {best_mAP}")
