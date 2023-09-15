7c5a55f56a9161760f32f94080d3f64addb9bab8
Astrophotographer <skcksdnr2@gmail.com>
2023-09-15 17:32:10 +0900

    0915 mailsend 추가인원 리스트 + 시간추가

--- web/mailsendTest.py
+++ web/mailsendTest.py
@@ -1,4 +1,12 @@
 from mailsend import *
+from datetime import datetime
 
 # test= Test()
-send_email(receiver='skcksdnr@naver.com', content= 'naver.com', title= '도난감지')
+receivers =['skcksdnr@naver.com',
+            # 이메일 주소입력,
+            # 이메일 주소입력,
+            ]
+
+send_email(receivers, content= 'naver.com', title= '도난감지', start_time=datetime.now())
+
+# datetime.now()에 변수 입력해주시면 됩니다.
